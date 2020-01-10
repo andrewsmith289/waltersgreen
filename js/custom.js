5948cdef1f6c603a117968683521a85b6373dbfb
andrewsmith289 <andrew.c.smith289@gmail.com>
2020-01-10 15:27:26 -0500

    removed unnecessary js.

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,3 @@
-// JavaScript Document
-
-
-
 	$(window).on('load', function() {
 	
 		"use strict";
@@ -34,53 +30,7 @@
 			$(".navbar").addClass("scroll");
 		} else {
 			$(".navbar").removeClass("scroll");
-		}
-		
-		
-		/*----------------------------------------------------*/
-		/*	Blue Circle Progress Bars
-		/*----------------------------------------------------*/
-	
-			if ($().easyPieChart) {
-				
-			var count = 0 ;
-			var colors = ['#25c6ff']; // Circle bar color
-
-			$('.percentage').each(function(){
-
-				var imagePos = $(this).offset().top;
-				var topOfWindow = $(window).scrollTop();
-				if (imagePos < topOfWindow+600) {
-
-					$(this).easyPieChart({
-						barColor: colors[count],
-						trackColor: 'rgba(20, 20, 20, .1)',
-						scaleColor: false,
-						scaleLength: false,
-						lineCap: 'butt',
-						rotate: 0,
-						lineWidth: 5,
-						size: 150,						
-						animate: 1350,
-						onStep: function(from, to, percent) {
-								$(this.el).find('.percent').text(Math.round(percent));
-							}
-							
-						});
-					
-					}
-
-					count++;
-					if (count >= colors.length) { count = 0};
-				
-				});
-
-			}
-				
-
-	});
-	
-	
+		}	
 	
 	$(document).ready(function() {
 		
@@ -103,8 +53,7 @@
 	
 		if ($('.navbar').hasClass('no-bg')) {
 			$('.intro-section').addClass('wide-intro')
-		}
-		
+		}		
 		
 		/*----------------------------------------------------*/
 		/*	Mobile Menu Toggle
@@ -113,21 +62,7 @@
 		$('.navbar-nav li.nav-link').on('click', function() {				
 			$('#navigation-menu').css("height", "1px").removeClass("in").addClass("collapse");
 			$('#navigation-menu').removeClass("open");				
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Countdown
-		/*----------------------------------------------------*/
-		
-		$("#clock").countdown("2017/03/08 20:00:00", function(event) {
-			$(this).html( event.strftime(''     
-				+ '<div class="cbox-1 clearfix"><span class="cbox-1-digit">%D</span> <span class="cbox-1-txt">Days</span></div>'
-				+ '<div class="cbox-1 clearfix"><span class="cbox-1-digit">%H</span> <span class="cbox-1-txt">Hrs</span></div>'
-				+ '<div class="cbox-1 clearfix"><span class="cbox-1-digit">%M</span> <span class="cbox-1-txt">Min</span></div>'
-				+ '<div class="cbox-1 clearfix"><span class="cbox-1-digit">%S</span> <span class="cbox-1-txt">Sec</span></div>'
-			));
-		});
+		});	
 				
 				
 		/*----------------------------------------------------*/
@@ -152,32 +87,6 @@
 	        }
 						
 		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Intro Typed Text
-		/*----------------------------------------------------*/
-		
-		$(".typed-element").typed({
-			strings: ["elegant", "responsive", "functional", "creative", "modern"]
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Intro Slider
-		/*----------------------------------------------------*/
-	
-		$('.intro_slider').flexslider({
-			animation: "fade",
-			controlNav: false,
-			directionNav: false,  
-			slideshowSpeed: 4000,   
-			animationSpeed: 700,  
-			start: function(slider){
-				$('body').removeClass('loading');
-			}
-		});
-
 		
 		/*----------------------------------------------------*/
 		/*	Animated Scroll To Anchor
@@ -264,170 +173,7 @@
 
 		};
 		
-		$.scrollUp();
-		
-		
-		/*----------------------------------------------------*/
-		/*	Video Background
-		/*----------------------------------------------------*/
-		
-		$('.video-play').vide("images/video/video", {
-			posterType: "jpg"
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Filterable Portfolio
-		/*----------------------------------------------------*/
-	
-		$("#portfolio-1-2 .row, #portfolio-2-2 .row, #portfolio-2-3 .row").mixitup({
-			targetSelector: '.portfolio-item',
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Single Image Lightbox
-		/*----------------------------------------------------*/
-				
-		 $('.image-link').magnificPopup({
-		  type: 'image'
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Images Gallery Lightbox
-		/*----------------------------------------------------*/
-		
-		$('.gallery-item').magnificPopup({
-		  type: 'image',
-		  gallery:{
-			enabled:true
-		  }
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Video Link Lightbox
-		/*----------------------------------------------------*/
-		
-		$('.video-popup').magnificPopup({
-		    type: 'iframe',
-		  	  
-				iframe: {
-					patterns: {
-						youtube: {
-			   
-							index: 'youtube.com',
-							src: 'https://www.youtube.com/embed/kuceVNBTJio'
-				
-								}
-							}
-						}		  		  
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Statistic Counter
-		/*----------------------------------------------------*/
-	
-		$('.statistic-number').each(function () {
-			$(this).appear(function() {
-				$(this).prop('Counter',0).animate({
-					Counter: $(this).text()
-				}, {
-					duration: 4000,
-					easing: 'swing',
-					step: function (now) {
-						$(this).text(Math.ceil(now));
-					}
-				});
-			},{accX: 0, accY: 0});
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Our Clients Carousel
-		/*----------------------------------------------------*/
-	
-		$(".clients-logo-carousel").owlCarousel({
-					  
-			slideSpeed : 1200,
-			items : 5,
-			itemsDesktop : [1199,6],
-			itemsDesktopSmall : [991,4],
-			itemsTablet: [767,4],
-			itemsMobile : [479,2],
-			navigation:true,
-			pagination:false,
-			navigationText : false
-					  
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Screens Carousel
-		/*----------------------------------------------------*/
-		
-		$(".screens_carousel").owlCarousel({
-					  
-			slideSpeed : 600,
-			items : 4,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [991,2],
-			itemsTablet: [768,2],
-			itemsMobile : [479,1],
-			navigation: true,
-			pagination: true,
-			navigationText : false
-					  
-		});
-				
-		// Carousel Navigation
-		$(".next").on('click', function(){
-			$(".screens_carousel").trigger('owl.next');
-		})
-		
-		$(".prev").on('click', function(){
-			$(".screens_carousel").trigger('owl.prev');
-		})
-		
-		
-		
-		/*----------------------------------------------------*/
-		/*	Blog Articles Rotator
-		/*----------------------------------------------------*/
-	
-		$(".blog-posts-holder").owlCarousel({
-			
-			autoPlay : true,
-			slideSpeed : 1000,
-			items : 3,
-			itemsDesktop : [1199,2],
-			itemsDesktopSmall : [991,2],
-			itemsTablet: [768,2],
-			itemsMobile : [480,1],
-			navigation: false,
-			pagination: true,
-			navigationText : false
-					  
-		});
-		
-		
-		/*----------------------------------------------------*/
-		/*	Testimonials Rotator
-		/*----------------------------------------------------*/
-		
-		$('.flexslider').flexslider({
-			animation: "fade",
-			controlNav: true,
-			directionNav: false,  
-			slideshowSpeed: 5000,   
-			animationSpeed: 2000,  
-			start: function(slider){
-				$('body').removeClass('loading');
-			}
-		});	
-		
+		$.scrollUp();	
 		
 		/*----------------------------------------------------*/
 		/*	Contact Form Validation
@@ -532,27 +278,6 @@
 						}
 		});
 		
-		
-		/*----------------------------------------------------*/
-		/*	Newsletter Subscribe Form
-		/*----------------------------------------------------*/
-	
-		$('.newsletter-form').ajaxChimp({
-        language: 'cm',
-        url: 'http://dsathemes.us3.list-manage.com/subscribe/post?u=af1a6c0b23340d7b339c085b4&id=344a494a6e'
-            //http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
-		});
-
-
-		$.ajaxChimp.translations.cm = {
-			'submit': 'Submitting...',
-			0: 'We have sent you a confirmation email',
-			1: 'Please enter a value',
-			2: 'An email address must contain a single @',
-			3: 'The domain portion of the email address is invalid (the portion after the @: )',
-			4: 'The username portion of the email address is invalid (the portion before the @: )',
-			5: 'This email address looks fake or invalid. Please enter a real email address'
-		};
 		
 					
 	});		
